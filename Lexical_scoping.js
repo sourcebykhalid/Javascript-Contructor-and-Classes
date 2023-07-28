cb43cc055269cb4c2c99fbe8c4da50cca7974dff
Khalid Shakeel <khalidshakeel46@gmail.com>
2023-07-28 11:27:26 +0530

    Lexical Scoping in Javascript

--- Lexical_scoping.js
+++ Lexical_scoping.js
@@ -1,14 +1,32 @@
 // Lexical Scoping in Javascript
 // In the below code snippet the inner func() can access the elements of outer func(), But the outer func() dont have the access of inner func(). But both functions inner() and outer() have the access of Global variables declared outside of the outer function scope
-let password = 74335;
-function outer() {
-  let user = "Alex";
-  // console.log(iD);
-  function inner() {
-    console.log(password);
-    let iD = 8635672;
-    console.log(user);
+// let password = 74335;
+// function outer() {
+//   let user = "Alex";
+//   // console.log(iD);
+//   function inner() {
+//     console.log(password);
+//     let iD = 8635672;
+//     console.log(user);
+//   }
+//   inner();
+// }
+// outer();
+
+let subj = "swift";
+function parent() {
+  let name = "MIke";
+  function child1() {
+    let rank = 4;
+    console.log(name);
+    console.log(subj);
   }
-  inner();
+  child1();
+  function child2() {
+    console.log(name);
+    // console.log(rank);
+    console.log(subj);
+  }
+  child2();
 }
-outer();
+parent();
